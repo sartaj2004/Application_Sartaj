81165934f569ce89ca1869efb4a56d87e5c4e14d
Sartaj Singh <sin14136@sheridancollege.ca>
2024-02-22 09:32:49 -0500

    Header and footer componenet completed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  currentDate: Date;
 
+  constructor() {
+    this.currentDate = new Date();
+  }
 }
